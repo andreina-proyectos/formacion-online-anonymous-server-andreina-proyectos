a9325ae3fcff714344b9ea2f7d60c751c4296f8d
Andreina-Romero <andreina.romero.garcia@gmail.com>
2019-11-12 19:34:16 +0100

    Add function to collapsables

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 'use strict';
+const burguerMenu = document.querySelector('.nav-trigger');
+const navList = document.querySelector('.nav');
+const navListClose = document.querySelector(".nav-close");
+
+function handleClickMenu () {
+  navList.classList.remove('hidden');
+  burguerMenu.classList.add('hidden');
+
+}
+
+function handleClickClose () {
+  navList.classList.add('hidden');
+  burguerMenu.classList.remove('hidden');
+}
+
+burguerMenu.addEventListener('click', handleClickMenu);
+navListClose.addEventListener('click', handleClickClose);
