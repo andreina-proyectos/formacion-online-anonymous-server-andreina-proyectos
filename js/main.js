a438276d768bf630a4305bb2bc2f272e271035c0
Andreina-Romero <andreina.romero.garcia@gmail.com>
2019-11-12 22:43:39 +0100

    Fix ovewflow on mobile device

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@ const burguerMenu = document.querySelector('.nav-trigger');
 const navList = document.querySelector('.nav');
 const navListClose = document.querySelector(".nav-close");
 
+var body = document.body;
+body.addEventListener('touchmove', function(e) {
+  e.preventDefault();
+}, false);
+
 function handleClickMenu () {
   navList.classList.remove('hidden');
   burguerMenu.classList.add('hidden');
